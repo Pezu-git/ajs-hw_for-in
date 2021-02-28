198f5510c42b6f18288395cc2f400b9e14bc5744
Pezu-git <artem_nikolaev69@mail.ru>
2021-03-01 00:43:09 +0300

    first commit

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,86 +1,44 @@
 /* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+import orderByProps from '../index.js';
 
-// import Daemon from '../js/Daemon.js';
-// import Bowerman from '../js/Bowerman.js';
-// import Magician from '../js/Magician.js';
-// import Swordsman from '../js/Swordsman.js';
-// import Undead from '../js/Undead.js';
-// import Zombie from '../js/Zombie.js';
-
-// test('first test', () => {
-//   const daemon = new Daemon('hif', 'Daemon');
-//   const receverDaemon = daemon.getChar();
-//   const expectedDaemon = {
-//     name: 'hif',
-//     type: 'Daemon',
-//     health: 100,
-//     level: 1,
-//     attack: 10,
-//     defence: 40,
-//   };
-//   expect(receverDaemon).toStrictEqual(expectedDaemon);
-//   const bowerman = new Bowerman('hif', 'Bowerman');
-//   const receverBowerman = bowerman.getChar();
-//   const expectedBowerman = {
-//     name: 'hif',
-//     type: 'Bowerman',
-//     health: 100,
-//     level: 1,
-//     attack: 25,
-//     defence: 25,
-//   };
-//   expect(receverBowerman).toStrictEqual(expectedBowerman);
-
-//   const magician = new Magician('hif', 'Magician');
-//   const receverMagician = magician.getChar();
-//   const expectedMagician = {
-//     name: 'hif',
-//     type: 'Magician',
-//     health: 100,
-//     level: 1,
-//     attack: 10,
-//     defence: 40,
-//   };
-//   expect(receverMagician).toStrictEqual(expectedMagician);
-//   const swordsman = new Swordsman('hif', 'Swordsman');
-//   const receverSwordsman = swordsman.getChar();
-//   const expectedSwordsman = {
-//     name: 'hif',
-//     type: 'Swordsman',
-//     health: 100,
-//     level: 1,
-//     attack: 40,
-//     defence: 10,
-//   };
-//   expect(receverSwordsman).toStrictEqual(expectedSwordsman);
-//   const undead = new Undead('hif', 'Undead');
-//   const receverUndead = undead.getChar();
-//   const expectedUndead = {
-//     name: 'hif',
-//     type: 'Undead',
-//     health: 100,
-//     level: 1,
-//     attack: 25,
-//     defence: 25,
-//   };
-//   expect(receverUndead).toStrictEqual(expectedUndead);
-//   const zombie = new Zombie('hif', 'Zombie');
-//   const receverZombie = zombie.getChar();
-//   const expectedZombie = {
-//     name: 'hif',
-//     type: 'Zombie',
-//     health: 100,
-//     level: 1,
-//     attack: 40,
-//     defence: 10,
-//   };
-//   expect(receverZombie).toStrictEqual(expectedZombie);
-//   const zombie1 = new Zombie('Zombie', 'Zombie1');
-//   const receverZombie1 = zombie1.getError('Не подходящее имя класса');
-//   const expectedZombie1 = console.error('Не подходящее имя класса');
-//   expect(receverZombie1).toStrictEqual(expectedZombie1);
-//   const zombie2 = new Zombie('ZombieZombie', 'Zombie');
-//   const receverZombie2 = zombie2.getError('Не подходящее имя');
-//   const expectedZombie2 = console.error('Не подходящее имя');
-//   expect(receverZombie2).toStrictEqual(expectedZombie2);
-// });
+test('first test', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const orderArr = ['name', 'level'];
+  const receverCall = orderByProps(obj, orderArr);
+  const expectObj = [
+   { prop: 'name', value: 'мечник' },
+    { prop: 'level', value: 2 },
+    { prop: 'attack', value: 80 },
+    { prop: 'defence', value: 40 },
+    { prop: 'health', value: 10 },
+  ];
+  expect(receverCall).toEqual(expectObj);
+});
+test('first testa', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 60,
+    // eslint-disable-next-line no-dupe-keys
+    attack: 80,
+  };
+  const orderArr = ['name', 'level'];
+  const receverCall = orderByProps(obj, orderArr);
+  const expectObj = [
+    { prop: 'name', value: 'мечник' },
+    { prop: 'level', value: 2 },
+    { prop: 'attack', value: 80 },
+    { prop: 'defence', value: 60 },
+    { prop: 'health', value: 10 },
+  ];
+  expect(receverCall).toEqual(expectObj);
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-
+export default function orderByProps(sortObj, order) {
+  const alfaBatOrder = [];
+  const keyOrder = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const prop in sortObj) {
+    if (!order.includes(prop)) {
+      alfaBatOrder.push({ prop, value: sortObj[`${prop}`] });
+    } else {
+      keyOrder.push({ prop, value: sortObj[`${prop}`] });
+    }
+  }
+  alfaBatOrder.sort((a, b) => {
+    if (a.prop > b.prop) {
+      return 1;
+    }
+    if (a.prop < b.prop) {
+      return -1;
+    }
+    return 0;
+  });
+  return [...keyOrder, ...alfaBatOrder];
+}
