3e0854cb61d22a9e9499d69192628b74c7f820b5
Pezu-git <artem_nikolaev69@mail.ru>
2021-03-01 09:55:48 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
+/* eslint-disable no-restricted-syntax */
 export default function orderByProps(sortObj, order) {
   const alfaBatOrder = [];
   const keyOrder = [];
-  // eslint-disable-next-line no-restricted-syntax
+  // eslint-disable-next-line guard-for-in
   for (const prop in sortObj) {
     if (!order.includes(prop)) {
       alfaBatOrder.push({ prop, value: sortObj[`${prop}`] });
     } else {
-      keyOrder.push({ prop, value: sortObj[`${prop}`] });
+      keyOrder[order.indexOf(`${prop}`)] = { prop, value: sortObj[`${prop}`] };
     }
+    alfaBatOrder.sort((a, b) => a.prop.localeCompare(b.prop));
   }
-  alfaBatOrder.sort((a, b) => {
-    if (a.prop > b.prop) {
-      return 1;
-    }
-    if (a.prop < b.prop) {
-      return -1;
-    }
-    return 0;
-  });
   return [...keyOrder, ...alfaBatOrder];
 }
